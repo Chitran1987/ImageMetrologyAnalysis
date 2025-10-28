041856c63b33295fbc110d830c15b8bd9c0f3c00
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-10-28 02:26:16 +0100

    Updated storage mode of M as double

--- R/normalize.R
+++ R/normalize.R
@@ -1,5 +1,5 @@
 normalize <- function(M){
-  #N <- M
+  storage.mode(M) <- "double"
   res <- .C("lineval", X = M, m = as.integer(dim(M)[1]), n = as.integer(dim(M)[2]), low = as.double(0.0), hi = as.double(1.0))
   return(res$X)
 }
