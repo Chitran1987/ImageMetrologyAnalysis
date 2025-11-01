12e6ef9216b4525e9e7a554eaee4504db6cec01f
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-11-01 16:47:48 +0100

    Help files for solve_sym_auto.R
    Additionally implemented list(markdown = TRUE) in my description file

--- R/solve_sym_auto.R
+++ R/solve_sym_auto.R
@@ -1,5 +1,14 @@
 # Solve A x = b. Works for vector or matrix b.
 solve_sym_auto <- function(A, b) {
+  if(!is.numeric(A) || !is.numeric(b)){
+    stop('A and b need to be of type numeric')
+  }
+  if(dim(A)[1] != dim(A)[2]){
+    stop('A needs to be a square matrix, please check dimensions')
+  }
+  if(dim(A)[1] != length(b)){
+    stop('Sizes of A and b are not compatible')
+  }
   storage.mode(A) <- "double"
   storage.mode(b) <- "double"
   # Try Cholesky (fast, if SPD). If it fails, fall back to LU.
