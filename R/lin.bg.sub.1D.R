9df0f96fc0ad3fe3681fd27fc171464c1ae13bae
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-06 12:18:22 +0100

    fortran_expose module written to expose my fortran_functions

---
+++ R/lin.bg.sub.1D.R
@@ -0,0 +1,26 @@
+###Write a function for linear background substraction in 1D
+lin.bg.sub.1D <- function(dat, win, min.zero){
+  ##error checking
+  #
+  #
+  #
+  #
+  #ensure that min.zero is an integer
+  #
+  #
+  #error checking
+  dat <- as.matrix(dat)
+  win <- as.matrix(win)
+  min.zero <- as.integer(min.zero)
+  storage.mode(dat) <- "double"
+  storage.mode(win) <- "double"
+  storage.mode(min.zero) <- 'integer'
+  dat.m = dim(dat)[1]
+  dat.n = dim(dat)[2]
+  win.m = dim(win)[1]
+  win.n = dim(win)[2]
+  #compute sizes for res_m and res_n
+  #don't need to compute they have the exact same size as the input matrix dat
+  res <- .C('lin_bg_sub_1D_c', dat=dat, dat_m=dat.m, dat_n=dat.n, win=win, win_m=win.m, win_n=win.n, res=dat, min_zero=min.zero)
+  return(res$res)
+}
---
+++ src/fortran_expose.f90
@@ -0,0 +1,17 @@
+module fortran_expose
+use iso_fortran_env, only: real64
+use fortran_utils
+implicit none
+public
+
+contains
+subroutine lin_bg_sub_1D_c(dat, dat_m, dat_n, win, win_m, win_n, res, min_zero) bind(C, name="lin_bg_sub_1D_c")
+  integer :: dat_m, dat_n, win_m, win_n
+  real(real64) :: dat(dat_m, dat_n), win(win_m, win_n), res(dat_m, dat_n)
+  integer :: min_zero
+  res = lin_bg_sub_1D(dat, win, logical(min_zero))
+end subroutine lin_bg_sub_1D_c
+
+
+
+end module fortran_expose
