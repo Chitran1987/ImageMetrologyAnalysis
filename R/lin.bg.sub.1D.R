27b37b629502ca0ed3e1e09e3cfc9ce0e1c1332e
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-06 16:57:57 +0100

    Documentation generated for lin.bg.sub.1D.R

--- R/lin.bg.sub.1D.R
+++ R/lin.bg.sub.1D.R
@@ -1,4 +1,61 @@
-###Write a function for linear background substraction in 1D
+#' Linear background subtraction
+#'
+#' @description Subtracts a linear background from line profiles.\cr
+#' Based on the input dataset from which the background is to be subtracted and...\cr
+#' the windows over which the background is to be calculated
+#'
+#'@usage lin.bg.sub.1D(dat, win, min.zero)
+#'lin.bg.sub.1D(dat, win)
+#'
+#' @param dat The dataset from which the background is to be subtracted. \cr
+#' The format for the entry is shown below
+#' * Must be a two column numeric dataframe
+#' * First column must be the X values and the 2nd column the Y values
+#'
+#' @param win The windows/subsets w.r.t X over which the background is to be calculated for subtraction. \cr
+#' The format for the entry is shown below
+#' * Must be a two column numeric dataframe
+#' * Each row consists of the minimum(1st column) and maximum(2nd column) X-values of a two column dataframe
+#' * Each minimum value should be less than each max value in the same row. Else an error is thrown.
+#' * Please see \link{Examples}
+#'
+#' @param min.zero A boolean bit which when set to \code{TRUE} ensures that the minimum value of the returned dataframe's Y values will be clamped at zero.\cr
+#'  Defaults to \code{TRUE}
+#'
+#'
+#' @return A Two column dataframe similar in shape, size, topology to the \code{dat} dataframe
+#'
+#' @author
+#' Chitran Ghosal <ghosal.chitran@gmail.com>
+#'
+#' @examples
+#' #Call the relevant libraries
+#' library(StatsChitran)
+#' library(ImageMetrologyAnalysis)
+#'
+#' #build the dataset
+#' X <- seq(-10, 10, 0.1)
+#' Y <- gauss(X, sig = 2, probability = T)
+#' Y <- Y + rnorm(length(X), sd=0.005)
+#'
+#' #add bg
+#' Y <- Y + 0.02*X
+#' plot(X, Y)
+#' #figure out the windows over which to calculate the background
+#' ##window 1
+#' abline(v=c(-10, -5), col='red')
+#' ##window 2
+#' abline(v=c(5, 10), col='red')
+#' ## build the win dataframe
+#' win <- matrix(c(-10, -5, 5, 10), byrow = T, nrow = 2)
+#' win <- as.data.frame(win)
+#'
+#' #Call the lin.bg.sub.1D() function
+#' bg.sub.dat <- lin.bg.sub.1D(dat = data.frame(X, Y), win = win, min.zero = T )
+#' plot(bg.sub.dat$X, bg.sub.dat$Y)
+#'
+#'
+#' @export
 lin.bg.sub.1D <- function(dat, win, min.zero){
   ##error checking
   #
