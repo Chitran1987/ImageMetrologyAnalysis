7a4413afd11a63db94ec9c2d4ab71e5aca89378c
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-06 15:33:02 +0100

    Returned a df instead of a matrix

--- R/lin.bg.sub.1D.R
+++ R/lin.bg.sub.1D.R
@@ -22,5 +22,7 @@ lin.bg.sub.1D <- function(dat, win, min.zero){
   #compute sizes for res_m and res_n
   #don't need to compute they have the exact same size as the input matrix dat
   res <- .C('lin_bg_sub_1D_c', dat=dat, dat_m=dat.m, dat_n=dat.n, win=win, win_m=win.m, win_n=win.n, res=dat, min_zero=min.zero)
-  return(res$res)
+  res <- as.data.frame(res$res)
+  names(res) <- c('X', 'Y')
+  return(res)
 }
