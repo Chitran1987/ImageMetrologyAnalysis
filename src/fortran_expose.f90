c93135e8bf074e57062d1309c96205f38b95de43
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-06 12:28:18 +0100

    Type casting error removed from fortran_expose

--- src/fortran_expose.f90
+++ src/fortran_expose.f90
@@ -9,8 +9,14 @@ subroutine lin_bg_sub_1D_c(dat, dat_m, dat_n, win, win_m, win_n, res, min_zero) 
   integer :: dat_m, dat_n, win_m, win_n
   real(real64) :: dat(dat_m, dat_n), win(win_m, win_n), res(dat_m, dat_n)
   integer :: min_zero
-  min_zero = logical(min_zero)
-  res = lin_bg_sub_1D(dat, win, min_zero)
+  logical :: min_zero1
+  !Typecast min_zero to min_zero1
+    if ( min_zero == 1 ) then
+        min_zero1 = .true.
+    else
+        min_zero1 = .false.
+    end if
+  res = lin_bg_sub_1D(dat, win, min_zero1)
 end subroutine lin_bg_sub_1D_c
 
 
