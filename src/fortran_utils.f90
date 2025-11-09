e8ea35b38b0ad67a789186efb64a19e16774317e
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-09 01:11:31 +0100

    Only indentation changed. Nothing else touched

--- src/fortran_utils.f90
+++ src/fortran_utils.f90
@@ -33,185 +33,185 @@ contains
 
 !function produces a sequence
 subroutine seqn(st, en, len, X)
-real(real64), intent(in) :: st, en
-integer, intent(in) :: len
-real(real64) :: X(len)
-integer :: i
-real(real64) :: del
-del = (en-st)/(len-1)
-do i = 1, len
-X(i) = st + (i-1)*del
-end do
+    real(real64), intent(in) :: st, en
+    integer, intent(in) :: len
+    real(real64) :: X(len)
+    integer :: i
+    real(real64) :: del
+    del = (en-st)/(len-1)
+    do i = 1, len
+    X(i) = st + (i-1)*del
+    end do
 end subroutine seqn
 
 !function calculates the difference of subsequent nos in a vector
 subroutine diff(X, Y)
-real(real64), dimension(:), intent(in)::X
-real(real64) :: Y(size(X)-1)
-integer :: i
-do i = 1, size(Y)
-Y(i) = X(i+1) - X(i)
-end do
+    real(real64), dimension(:), intent(in)::X
+    real(real64) :: Y(size(X)-1)
+    integer :: i
+    do i = 1, size(Y)
+    Y(i) = X(i+1) - X(i)
+    end do
 end subroutine diff
 
 !function calculates the mean of a vector
 subroutine mean(X, y)
-real(real64), intent(in), dimension(:) :: X
-real(real64) :: y
-y = sum(X)/size(X)
+    real(real64), intent(in), dimension(:) :: X
+    real(real64) :: y
+    y = sum(X)/size(X)
 end subroutine mean
 
 !function calculates the var of a vector
 subroutine var(X, res)
-real(real64), intent(in), dimension(:) :: X
-real(real64) :: res
-real(real64) :: dmp = 0
-integer :: i
-real(real64) :: a
-call mean(X, a)
-do i = 1, size(X)
-dmp = dmp + (X(i) - a)**2
-end do
-!print *, dmp
-!print *, size(X)-1
-res = dmp/(size(X)-1)
-dmp = 0
+    real(real64), intent(in), dimension(:) :: X
+    real(real64) :: res
+    real(real64) :: dmp = 0
+    integer :: i
+    real(real64) :: a
+    call mean(X, a)
+    do i = 1, size(X)
+    dmp = dmp + (X(i) - a)**2
+    end do
+    !print *, dmp
+    !print *, size(X)-1
+    res = dmp/(size(X)-1)
+    dmp = 0
 end subroutine var
 
 !function calculates the sdev of a vector
 subroutine sdev(X, res)
-real(real64), intent(in), dimension(:) :: X
-real(real64) :: res, res1
-call var(X, res1)
-res = sqrt(res1)
+    real(real64), intent(in), dimension(:) :: X
+    real(real64) :: res, res1
+    call var(X, res1)
+    res = sqrt(res1)
 end subroutine sdev
 
 !function averages between successive elements of vector
 subroutine mdpnt_vec(X, res)
-real(real64), intent(in), dimension(:) :: X
-real(real64) :: res(size(X)-1)
-integer :: i, n
-n = size(X)
-do i = 1, n-1
-res(i) = (X(i) + X(i+1))/2
-end do
+    real(real64), intent(in), dimension(:) :: X
+    real(real64) :: res(size(X)-1)
+    integer :: i, n
+    n = size(X)
+    do i = 1, n-1
+    res(i) = (X(i) + X(i+1))/2
+    end do
 end subroutine mdpnt_vec
 
 !subroutine for reversing a real vector
 subroutine rev_real(X)
-real(real64) :: X(:)
-integer :: n, i
-real(real64) :: tmp
-n = size(X)
-do i = 1, n/2
-tmp = X(i)
-X(i) = X(n+1-i)
-X(n+1-i) = tmp
-end do
+    real(real64) :: X(:)
+    integer :: n, i
+    real(real64) :: tmp
+    n = size(X)
+    do i = 1, n/2
+    tmp = X(i)
+    X(i) = X(n+1-i)
+    X(n+1-i) = tmp
+    end do
 
 end subroutine rev_real
 
 !subroutine for reversing a complex vector
 subroutine rev_cmplx(X)
-complex(real64) :: X(:)
-integer :: n, i
-complex(real64) :: tmp
-n = size(X)
-do i = 1, n/2
-tmp = X(i)
-X(i) = X(n+1-i)
-X(n+1-i) = tmp
-end do
+    complex(real64) :: X(:)
+    integer :: n, i
+    complex(real64) :: tmp
+    n = size(X)
+    do i = 1, n/2
+    tmp = X(i)
+    X(i) = X(n+1-i)
+    X(n+1-i) = tmp
+    end do
 
 end subroutine rev_cmplx
 
 !Subroutine for reversing an integer vector
 subroutine rev_int(X)
-integer :: X(:)
-integer n, i
-integer :: tmp
-n = size(X)
-do i = 1, n/2
-tmp = X(i)
-X(i) = X(n+1-i)
-X(n+1-i) = tmp
-end do
+    integer :: X(:)
+    integer n, i
+    integer :: tmp
+    n = size(X)
+    do i = 1, n/2
+    tmp = X(i)
+    X(i) = X(n+1-i)
+    X(n+1-i) = tmp
+    end do
 end subroutine rev_int
 
 !function for print_mat_real(M)
 subroutine print_mat_real(M)
-real(real64), intent(in) :: M(:,:)
-integer i, dmp
-do i = 1, size(M, 1)
-print *, M(i, :)
-end do
+    real(real64), intent(in) :: M(:,:)
+    integer i, dmp
+    do i = 1, size(M, 1)
+    print *, M(i, :)
+    end do
 end subroutine print_mat_real
 
 subroutine print_mat_int(M)
-integer, intent(in) :: M(:,:)
-integer i, dmp
-do i = 1, size(M, 1)
-print *, M(i, :)
-end do
+    integer, intent(in) :: M(:,:)
+    integer i, dmp
+    do i = 1, size(M, 1)
+    print *, M(i, :)
+    end do
 end subroutine print_mat_int
 
 subroutine print_mat_bool(M)
-logical, intent(in) :: M(:,:)
-integer i, dmp
-do i = 1, size(M, 1)
-print *, M(i, :)
-end do
+    logical, intent(in) :: M(:,:)
+    integer i, dmp
+    do i = 1, size(M, 1)
+    print *, M(i, :)
+    end do
 end subroutine print_mat_bool
 
 subroutine print_mat_cmplx(M)
-complex(real64), intent(in) :: M(:,:)
-integer i, dmp
-do i = 1, size(M, 1)
-print *, M(i, :)
-end do
+    complex(real64), intent(in) :: M(:,:)
+    integer i, dmp
+    do i = 1, size(M, 1)
+    print *, M(i, :)
+    end do
 end subroutine print_mat_cmplx
 
 subroutine lineval_r(X, m, n, low, hi) bind(C, name="lineval")
-integer, intent(in) :: m, n
-real(real64) :: X(m, n)
-real(real64), intent(in) :: low, hi
-X = ((hi - low)/(maxval(X) - minval(X)))*(X - minval(X)) + low
+    integer, intent(in) :: m, n
+    real(real64) :: X(m, n)
+    real(real64), intent(in) :: low, hi
+    X = ((hi - low)/(maxval(X) - minval(X)))*(X - minval(X)) + low
 end subroutine lineval_r
 
 subroutine lin_reg(dat, p, q, Coeff) bind(C, name="lin_reg")
-        integer :: p, q
-        real(real64) :: dat(p, q)
-        real(real64) :: Coeff(q)
-        real(real64), allocatable :: A(:),M(:,:), dmp(:)
-        integer :: i, j, info
-        !p = size(dat, 1)
-        !q = size(dat, 2)
-        !allocate(Coeff(q))
-        allocate(A(q))
-        allocate(dmp(q))
-        allocate(M(q,q)) !This is a symmetric matrix, barring the last row and column
-        do concurrent (j=1:q-1, i=1:q-1, i>=j) !Fill the first half
-            A(j) = dot_product(dat(:,j),dat(:,q))
-            M(i,j) = dot_product(dat(:,i), dat(:,j))
-        end do
-        do concurrent (j=2:q-1, i=1:q-1, i<j) !Write to the 2nd half
-            M(j,i) = M(i,j)
-        end do
-        A(q) = sum(dat(:,q))
-        do i = 1, q-1
-            dmp(i) = sum(dat(:,i))
-        end do
-        dmp(q) = real(p, kind=real64)
-        M(:,q) = dmp
-        M(q,:) = dmp
-        call solve_linear_system(A = M, b = A, info = info)
-        if ( info == 0 ) then
-            Coeff = A
-        else if ( info > 0 ) then
-            error stop "Matrix M is singular — cannot invert."
-        else
-            error stop "Invalid arguments passed - please check arguments"
-        end if
+    integer :: p, q
+    real(real64) :: dat(p, q)
+    real(real64) :: Coeff(q)
+    real(real64), allocatable :: A(:),M(:,:), dmp(:)
+    integer :: i, j, info
+    !p = size(dat, 1)
+    !q = size(dat, 2)
+    !allocate(Coeff(q))
+    allocate(A(q))
+    allocate(dmp(q))
+    allocate(M(q,q)) !This is a symmetric matrix, barring the last row and column
+    do concurrent (j=1:q-1, i=1:q-1, i>=j) !Fill the first half
+        A(j) = dot_product(dat(:,j),dat(:,q))
+        M(i,j) = dot_product(dat(:,i), dat(:,j))
+    end do
+    do concurrent (j=2:q-1, i=1:q-1, i<j) !Write to the 2nd half
+        M(j,i) = M(i,j)
+    end do
+    A(q) = sum(dat(:,q))
+    do i = 1, q-1
+        dmp(i) = sum(dat(:,i))
+    end do
+    dmp(q) = real(p, kind=real64)
+    M(:,q) = dmp
+    M(q,:) = dmp
+    call solve_linear_system(A = M, b = A, info = info)
+    if ( info == 0 ) then
+        Coeff = A
+    else if ( info > 0 ) then
+        error stop "Matrix M is singular — cannot invert."
+    else
+        error stop "Invalid arguments passed - please check arguments"
+    end if
 end subroutine lin_reg
 
 function lin_bg_sub_1D(dat, win, min_zero) result(ret_mat)
