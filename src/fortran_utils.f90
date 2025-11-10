656b4f9d0b31e096f3ed06b3de7a9f187d1258ea
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-11 00:23:15 +0100

    fortran_utils upgraded with integrate()

--- src/fortran_utils.f90
+++ src/fortran_utils.f90
@@ -382,5 +382,47 @@ function lin_prof_h(M, h_val) result(res_mat)
     res_mat(:,2) = M(imin(1),:,1)
 end function lin_prof_h
 
+! Integrates a 1D function between X and Y within limits xmin and xmax
+function integrate(X, Y, xmin, xmax, Riemann)
+    real(real64), intent(in), dimension(:) :: X, Y
+    real(real64) :: integrate
+    real(real64), dimension(:), allocatable :: X_sub, Y_sub
+    real(real64) :: xmin, xmax
+    real(real64) :: sum
+    logical, dimension(:), allocatable :: mask
+    integer :: i
+    logical, optional :: Riemann
+    logical :: Riemann1
+    mask = ( (X >= xmin) .and. (X <= xmax) )
+    X_sub = pack(X, mask)
+    Y_sub = pack(Y, mask)
+
+    !Set Default Riemann argument to true
+    if ( .not. present(Riemann) ) then
+        Riemann1 = .true.
+    else
+        Riemann1 = Riemann
+    end if
+
+    !If Riemann Integratable
+    if ( Riemann1 ) then
+        sum = 0
+        do i = 1, size(X_sub)-1
+            sum = sum + Y_sub(i)*(X_sub(i+1)-X_sub(i))
+        end do
+        integrate = sum
+        sum = 0
+    else
+        sum = 0
+        do i = 1, size(X_sub)-1
+            sum = sum + Y_sub(i)*(X_sub(i+1)-X_sub(i)) ! The Riemann rectangle
+            sum = sum + 0.5*(Y_sub(i+1) - Y_sub(i))*(X_sub(i+1) - X_sub(i))
+        end do
+        integrate = sum
+        sum = 0
+    end if
+
+end function integrate
+
 end module fortran_utils
 
