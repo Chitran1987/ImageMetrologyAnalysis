9844178e9f0be4fec4d140c2dd931c39cd955ebd
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-10 12:58:12 +0100

    Syntax errors removed

--- R/lin.prof.h.R
+++ R/lin.prof.h.R
@@ -5,8 +5,8 @@ lin.prof.h <- function(tens, h.val){
   if( !is.numeric(h.val) || length(tens) != 1 ){
     stop('h.val should be a numeric scalar')
   }
-  storage.mode('tens') <- 'double'
-  storage.mode('h.val') <- 'double'
+  storage.mode(tens) <- 'double'
+  storage.mode(h.val) <- 'double'
   res.mat <- matrix(data = 0, nrow = dim(tens)[2], ncol = 2)
   res <- .C("lin_prof_h_c", tens, dim(tens)[1], dim(tens)[2], dim(tens)[3], h.val, res.mat)
   res <- as.data.frame(res$res.mat)
