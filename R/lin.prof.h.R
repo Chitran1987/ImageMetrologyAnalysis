46ce1c412663001f5a113e98efec6185fc9c30e4
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-10 13:22:13 +0100

    changed again

--- R/lin.prof.h.R
+++ R/lin.prof.h.R
@@ -5,11 +5,13 @@ lin.prof.h <- function(tens, h.val){
   if( !is.numeric(h.val) || length(h.val) != 1 ){
     stop('h.val should be a numeric scalar')
   }
-  res_mat <- matrix(data = 0, nrow = dim(tens)[2], ncol = 2)
+  res_mat <- matrix(data = 0.0, nrow = dim(tens)[2], ncol = 2)
+  v <- dim(tens)
   storage.mode(tens) <- 'double'
   storage.mode(h.val) <- 'double'
   storage.mode(res_mat) <- 'double'
-  res <- .C("lin_prof_h_c", tens, dim(tens)[1], dim(tens)[2], dim(tens)[3], h.val, res_mat)
+  storage.mode(v) <- 'integer'
+  res <- .C("lin_prof_h_c", tens, v[1], v[2], v[3], h.val, res_mat)
   #res <- as.data.frame(res$res.mat)
   #names(res) <- c('dist', 'magnitude')
   return(res$res_mat)
