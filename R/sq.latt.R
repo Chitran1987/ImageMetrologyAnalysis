1f7fff52ce4fc2b7457e0081d61d90c2c2c49113
Chitran1987 <ghosal.chitran@gmail.com>
2025-11-18 14:30:08 +0100

    storage mode quirk changed/updated

--- R/sq.latt.R
+++ R/sq.latt.R
@@ -7,14 +7,14 @@ sq.latt <- function(X, Y, R.latt, A, sig, pl = T){
   m.y <- length(Y)
   tens <- array(data = 0, dim = c(m.y, m.x, 3))
   #Define the storage modes for passing to fortran
-  storage.mode('X') <- 'double'
-  storage.mode('Y') <- 'double'
-  storage.mode('R.latt') <- 'double'
-  storage.mode('A') <- 'double'
-  storage.mode('sig') <- 'double'
-  storage.mode('tens') <- 'double'
-  storage.mode('m.x') <- 'integer'
-  storage.mode('m.y') <- 'integer'
+  storage.mode(X) <- 'double'
+  storage.mode(Y) <- 'double'
+  storage.mode(R.latt) <- 'double'
+  storage.mode(A) <- 'double'
+  storage.mode(sig) <- 'double'
+  storage.mode(tens) <- 'double'
+  storage.mode(m.x) <- 'integer'
+  storage.mode(m.y) <- 'integer'
   res.list <- .C('square_latt_sb_c', X = X, Y = Y, mx = m.x, my = m.y, R_latt = R.latt, A = A, sig = sig, tens = tens)
   ret.tens <- res.list$tens
   if(pl){
