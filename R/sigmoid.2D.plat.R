d551930f614f78a9cf234b89caccffa0861e678d
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-12-26 23:43:42 +0100

    Sigmoid.2D.plat() function manual upload attempt

--- R/sigmoid.2D.plat.R
+++ R/sigmoid.2D.plat.R
@@ -1,4 +1,46 @@
-###function sigmoid.2D.plat
+#' The sigmoid.2D.plat function
+#'
+#' @description The sigmoid plateau function in two dimensions
+#' @usage sigmoid.2D.plat(X, Y, k, x.left, x.right, y.left, y.right, pl)
+#' sigmoid.2D.plat(X, Y, k, x.left, x.right, y.left, y.right)
+#'
+#' @param X,Y The numeric vectors which create the XY grid
+#'
+#'
+#' @param k The sharpness \eqn{k} of the sigmoid function
+#'  * higher \eqn{k} implies a sharper cutoff
+#'
+#' @param x.left,x.right,y.left,y.right The cutoff values along the X and Y directions
+#'
+#' @param pl Plots the 2D sigmoid plateau when set to \code{TRUE}\cr
+#' Defaults to \code{TRUE}
+#'
+#'
+#' @details
+#' The function returns a numeric matrix \code{Z(n,m)}\cr
+#' \code{n = size(Y)}\cr
+#' \code{m = size(X)}\cr
+#' The matrix is built as a product of sigmoid plateaus running along the orthogonal X and Y directions\cr
+#'
+#'
+#'
+#' @return The returned value is always a numeric matrix
+#'
+#' @author
+#' Chitran Ghosal <ghosal.chitran@gmail.com>
+#'
+#' @examples
+#' #Call the relevant libraries
+#' library(ImageMetrologyAnalysis)
+#' library(StatsChitran)
+#' #Create the sequences
+#' X <- seq(-10, 10, by= 0.01)
+#' Y <- seq(-7, 7, by=0.01)
+#' #Create the plateau matrix and plot it
+#' Z <- sigmoid.2D.plat(X, Y, k=3, x.left = -8, x.right = 8, y.left = -3, y.right = 3)
+#'
+#'
+#' @export
 sigmoid.2D.plat <- function(X, Y, k, x.left, x.right, y.left, y.right, pl = T){
   library(StatsChitran)
   ###error##########################################
