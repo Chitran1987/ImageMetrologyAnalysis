95d1b99dc8bcfa9561e45259a76693465d77410c
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-12-26 21:13:50 +0100

    Updated/Uploaded manual for sigmoid.plat()

--- R/sigmoid.plat.R
+++ R/sigmoid.plat.R
@@ -1,4 +1,45 @@
-####Create the sigmoid plateau function
+#' The simplified sigmoid function
+#'
+#' @description The sigmoid plateau function in one dimension
+#' @usage sigmoid.plat(X, k, left.cut, right.cut)
+#'
+#' @param X The numeric vector which contains the \eqn{x} values against which the sigmoid vector is needed
+#'
+#' @param k The sharpness \eqn{k} of the sigmoid function
+#'  * higher \eqn{k} implies a sharper cutoff
+#'
+#' @param left.cut The cutoff value \eqn{x_1} at the left side of the sigmoid plateau
+#'
+#' @param right.cut The cutoff value \eqn{x_2} at the right side of the sigmoid plateau
+#'
+#'
+#' @details
+#' The function returns a vector \eqn{\large \bar{Y}} with individual components \eqn{\large y} against the vector \eqn{\large \bar{X}} with component \eqn{\large x}
+#' \deqn{\Large y = \{1/(1 + e^{-k(x - x_1)})\} - \{1/(1 + e^{-k(x - x_2)})\} }
+#'
+#'
+#'
+#' @return The returned value is always a vector
+#'
+#' @author
+#' Chitran Ghosal <ghosal.chitran@gmail.com>
+#'
+#' @examples
+#' #Call the relevant libraries
+#' library(ImageMetrologyAnalysis)
+#'
+#' #Build the X-variable and then call the sigmoid.plat() function
+#' X <- seq(-10, 10, by=0.01)
+#' Y1 <- sigmoid.plat(X, k = 7, left.cut = -8, right.cut = 8)
+#' Y2 <- sigmoid.plat(X, k = 2, left.cut = -8, right.cut = 8)
+#'
+#' #Plot the curves
+#' plot(X, Y1, type = 'l', col='blue')
+#' lines(X, Y2, type = 'l', col='green')
+#' abline(v=c(-8,8), col='red')
+#'
+#'
+#' @export
 sigmoid.plat <- function(X, k, left.cut, right.cut){
   ###error checking
   #
