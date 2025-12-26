4a0ac79e66762dd146353e38f82a522c593992a6
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-12-26 23:14:38 +0100

    solved typo on sigmoid.2D.plat.R

--- R/sigmoid.2D.plat.R
+++ R/sigmoid.2D.plat.R
@@ -21,10 +21,10 @@ sigmoid.2D.plat <- function(X, Y, k, x.left, x.right, y.left, y.right, pl = T){
   storage.mode(n) <- 'integer'
   storage.mode(Z) <- 'double'
   storage.mode(k) <- 'double'
-  storage.mode(x_left) <- 'double'
-  storage.mode(y_left) <- 'double'
-  storage.mode(x_right) <- 'double'
-  storage.mode(y_right) <- 'double'
+  storage.mode(x.left) <- 'double'
+  storage.mode(y.left) <- 'double'
+  storage.mode(x.right) <- 'double'
+  storage.mode(y.right) <- 'double'
   fortran.res <- .C('sigmoid_2D_c',X = X, m = m, Y = Y, n = n, Z = Z, k = k, x_left = x.left, y_left = y.left, x_right = x.right, y_right = y.right)
   if(pl){
     plot2D.mat(X, Y, Z)
