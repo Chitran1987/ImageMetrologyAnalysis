e35e1b45dcdbb9b9fbd6ff03c2b082f99b5d167b
Chitran Ghosal <ghosal.chitran@gmail.com>
2025-12-29 01:39:20 +0100

    Built a new dataset called latt_reconstr_1axis_lite of only (201 X 201 X 3)

---
+++ R/data-latt_reconstr_1axis_lite.R
@@ -0,0 +1,8 @@
+#' A lattice showing reconstruction along one dimension
+#'
+#' A numeric 3D array of dimensions (201 × 201 × 3), generated internally
+#' and used for example testing of fft_2D_map() function.
+#'
+#' @format A numeric array with dimensions (201, 201, 3)
+#' @source Generated using internal R scripts
+"latt_reconstr_1axis_lite"
