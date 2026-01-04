05e0d58dd735542e35ac0dafc792a8f3bdfb7050
Chitran1987 <ghosal.chitran@gmail.com>
2026-01-04 15:52:29 +0100

    Created a help file for roxy_clean()

--- R/roxy_clean.R
+++ R/roxy_clean.R
@@ -1,3 +1,27 @@
+#' Cleans the manual files for roxygen2
+#'
+#' @description The function deletes the manual files for the package and then recreates them using \code{roxygen2::roxygenize()}
+#' @usage roxy_clean()
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#' @return The function returns nothing
+#'
+#' @author
+#' Chitran Ghosal <ghosal.chitran@gmail.com>
+#'
+#' @examples
+#' \dontrun{
+#' roxy_clean()
+#' }
+#'
+#'
+#'
+#' @export
 roxy_clean <- function() {
   unlink("man", recursive = TRUE)
   dir.create("man", showWarnings = FALSE)
