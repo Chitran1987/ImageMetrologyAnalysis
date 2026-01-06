41d2e1a5d3328b42fdc73d3c2e00ff8eeb5d85e8
Chitran Ghosal <ghosal.chitran@gmail.com>
2026-01-06 01:47:40 +0100

    Built manual files for plot2D.win.sig() function

--- R/plot2D.win.sig.R
+++ R/plot2D.win.sig.R
@@ -1,3 +1,53 @@
+#' Sigmoid based windowing of an image Tensor
+#'
+#' @description Window a certain area of a rank-3 image tensor using the sigmoid function.
+#'
+#' @usage plot2D.win.sig(tens, center, k, Xspan, Yspan, pl)
+#' plot2D.win.sig(tens, center, k, Xspan, Yspan)
+#'
+#' @param tens The image tensor of the form \eqn{\large T_{m,n,p=3}} that you want to window a part of
+#'
+#' @param center The co-ordinates of the center of the window
+#'  * Must be a 2-element numeric vector of the form \code{center = c(Xc, Yc)}
+#'  * Here \code{Xc} and \code{Yc} are the X and Y co-ordinates of the center
+#'
+#' @param k smoothness/sharpness parameter of the sigmoid function being used for the windowing
+#'  * Higher is sharper and lower is smoother/softer
+#'  * Please see \link{sigmoid.2D.plat}, \link{sigmoid.plat}
+#'
+#' @param Xspan,Yspan The spans around the center of the window. Numeric scalars.
+#'
+#' @param pl Boolean bit. Permission to plot the resulting window
+#' Defaults to \code{TRUE}
+#'
+#'
+#' @details
+#' Please see \link{sigmoid}, \link{sigmoid.plat}, \link{sigmoid.2D.plat} for a better understanding\cr
+#' Useful for windowing areas of interest in lattices for fourier/spectral analysis
+#'
+#'
+#'
+#' @return The returned value is always a rank-3 image tensor \eqn{\large T_{m,n,p=3}}\cr
+#' It has the exact same dimensions as the \code{tens} argument \cr
+#' It has the slice-2 and slice-3 matrixes untouched since they are the X-Y grid of the image
+#'
+#'
+#' @author
+#' Chitran Ghosal <ghosal.chitran@gmail.com>
+#'
+#' @examples
+#' #Call the relevant libraries
+#' library(StatsChitran)
+#' library(ImageMetrologyAnalysis)
+#'
+#' #Load the lattice
+#' data("latt_reconstr_1axis", package = "ImageMetrologyAnalysis")
+#' latt <- latt_reconstr_1axis
+#' plot2D.arr(latt)
+#' latt.zoom <- plot2D.win.sig(tens = latt, center = c(5,0), k = 10, Xspan = 8, Yspan = 2)
+#'
+#'
+#' @export
 plot2D.win.sig <- function(tens, center, k, Xspan, Yspan, pl = T){
   library(StatsChitran)
   #error checks
